5851dbccce783b30f5a5b7949beda36c3e214161
Mathias Schultz Rossen <math406c@edu.eal.dk>
2017-11-28 14:13:38 +0100

    Another small update to the skrotlog.js. Should now properly empty the new ContractLine

--- SkrotlogMVC/wwwroot/js/skrotlog.js
+++ SkrotlogMVC/wwwroot/js/skrotlog.js
@@ -4,8 +4,11 @@
         contractLine++;
         $(".contractLine:first").clone().appendTo(".contractLines");
         $(".contractLine:last .form-group select").attr("name", "contractLineArray[][" + contractLine + "]");
+        $(".contractLine:last .form-group select").attr("value", "");
         $(".contractLine:last .form-group input").attr("name", "contractLineArray[][" + contractLine + "]");
+        $(".contractLine:last .form-group input").attr("value", "");
         $(".contractLine:last .form-group textarea").attr("name", "contractLineArray[][" + contractLine + "]");
+        $(".contractLine:last .form-group textarea").html("");
         $(".contractLine:last").hide();
         $(".contractLine:last").slideToggle();
     });
