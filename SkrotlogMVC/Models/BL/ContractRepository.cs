e4c6cc51eab5d30ff987c4b2e032850ad77efa82
Søren Hansen <soer393c@edu.eal.dk>
2017-11-29 10:33:08 +0100

    Added singleton for ContractRepository. Made test contracts and lines.

--- SkrotlogMVC/Models/BL/ContractRepository.cs
+++ SkrotlogMVC/Models/BL/ContractRepository.cs
@@ -1,77 +1,97 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using SkrotlogMVC.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkrotlogMVC.Models;
 
-//namespace Skrotlog.BL
-//{
-//    public class ContractRepository
-//    {
-//        private List<Contract> listOfContracts;
-//        //private DALFacade dal;
+namespace SkrotlogMVC.Models.BL
+{
+    public class ContractRepository
+    {
+        private List<Contract> listOfContracts;
+        //private DALFacade dal;
 
-//        public int Count
-//        {
-//            get { return listOfContracts.Count; }
-//        }
+        public CustomerRepository CustomerRepositoryRef { get; set; }
 
-//        public ContractRepository()
-//        {
-//            //dal = DALFacade.Instance;
-//            //listOfContracts = dal.GetContracts();
-//        }
+        public int Count
+        {
+            get { return listOfContracts.Count; }
+        }
 
-//        public void AddContract(Customer customer, DateTime date, Currency currency, string initials)
-//        {
-//            //int newContractId = dal.GetHighestContractId() + 1;
-//            //Contract c = new Contract(newContractId ,customer, date, currency, initials);
-//            //listOfContracts.Add(c);
-//            //dal.AddContract(c);
-//        }
+        public ContractRepository()
+        {
+            //dal = DALFacade.Instance;
+            //listOfContracts = dal.GetContracts();
+            listOfContracts = new List<Contract>();
 
-//        public void AddContractLine(int contractId, Material material, decimal price, int amount, string comment)
-//        {
-//            //ContractLine c = new ContractLine(material, price, amount, comment);
-//            //listOfContracts.Find(x => x.Id == contractId).ContractLines.Add(c);
-//            //dal.AddContractLine(contractId, c);
-//        }
 
-//        public void AddAmount(int contractId, int contractLineId, int amount)
-//        {
-//            Contract selectedContract = listOfContracts.Find(x => x.Id == contractId);
-//            ContractLine selectedContractLine = selectedContract.ContractLines.Find(x => x.Id == contractLineId);
-//            selectedContractLine.DeliveredAmount += amount;
-//            dal.AddAmount(contractLineId, amount);
-//        }
+            //TEST
+            Contract c = new Contract(0, CustomerRepositoryRef.CustomerCollection.First(), DateTime.Now, Currency.DKK, "MR");
+            c.ContractLines.Add(new ContractLine(0, new Material("Jern", "j"), 200, 1000, 0, true, "whatever"));
+            c.ContractLines.Add(new ContractLine(0, new Material("Hur", "H"), 50, 200, 0, true, "whatever"));
+            c.ContractLines.Add(new ContractLine(0, new Material("Bananer", "b"), 2, 100, 0, true, "whatever"));
+            c.ContractLines.Add(new ContractLine(0, new Material("Fort", "f"), 20000, 100000, 0, true, "whatever"));
+            listOfContracts.Add(c);
+            c = new Contract(1, CustomerRepositoryRef.CustomerCollection.First(), DateTime.Now, Currency.EUR, "SH");
+            c.ContractLines.Add(new ContractLine(0, new Material("Jern", "j"), 200, 1000, 0, true, "whatever"));
+            c.ContractLines.Add(new ContractLine(0, new Material("Hur", "H"), 50, 200, 0, true, "whatever"));
+            c.ContractLines.Add(new ContractLine(0, new Material("Bananer", "b"), 2, 100, 0, true, ""));
+            c.ContractLines.Add(new ContractLine(0, new Material("Fort", "f"), 20000, 100000, 0, true, "Der er et fort her!"));
+            listOfContracts.Add(c);
 
-//        public List<Contract> GetCustomerContracts(int customerId)
-//        {
-//            return listOfContracts.FindAll(x => x.Customer.Id == customerId);
-//        }
 
-//        public List<Contract> GetCustomerContracts(string name)
-//        {
-//            return listOfContracts.FindAll(x => x.Customer.Name.ToLower() == name.ToLower());
-//        }
+        }
 
-//        public List<Contract> GetContracts()
-//        {
-//            return listOfContracts;
-//        }
+        public void AddContract(Customer customer, DateTime date, Currency currency, string initials)
+        {
+            //int newContractId = dal.GetHighestContractId() + 1;
+            Contract c = new Contract(customer, date, currency, initials);
+            listOfContracts.Add(c);
+            //dal.AddContract(c);
+        }
 
-//        public void DeactiveContractLine(int contractId, int contractLineId)
-//        {
-//            listOfContracts.Find(x => x.Id == contractId).ContractLines.Find(x => x.Id == contractLineId).Active = false;
-//            dal.DeactivateContractLine(contractLineId);
-//        }
+        public void AddContractLine(int contractId, Material material, decimal price, int amount, string comment)
+        {
+            //ContractLine c = new ContractLine(material, price, amount, comment);
+            //listOfContracts.Find(x => x.Id == contractId).ContractLines.Add(c);
+            //dal.AddContractLine(contractId, c);
+        }
 
-//        public void RemoveContractLine(int contractId, int contractLineId)
-//        {
-//            listOfContracts.Find(x => x.Id == contractId).ContractLines.RemoveAll(x => x.Id == contractLineId);
-//            dal.RemoveContractLine(contractLineId);
-//        }
+        public void AddAmount(int contractId, int contractLineId, int amount)
+        {
+            Contract selectedContract = listOfContracts.Find(x => x.Id == contractId);
+            ContractLine selectedContractLine = selectedContract.ContractLines.Find(x => x.Id == contractLineId);
+            selectedContractLine.DeliveredAmount += amount;
+            //dal.AddAmount(contractLineId, amount);
+        }
 
-//    }
-//}
+        public List<Contract> GetCustomerContracts(int customerId)
+        {
+            return listOfContracts.FindAll(x => x.Customer.Id == customerId);
+        }
+
+        public List<Contract> GetCustomerContracts(string name)
+        {
+            return listOfContracts.FindAll(x => x.Customer.Name.ToLower() == name.ToLower());
+        }
+
+        public List<Contract> GetContracts()
+        {
+            return listOfContracts;
+        }
+
+        public void DeactiveContractLine(int contractId, int contractLineId)
+        {
+            listOfContracts.Find(x => x.Id == contractId).ContractLines.Find(x => x.Id == contractLineId).Active = false;
+            //dal.DeactivateContractLine(contractLineId);
+        }
+
+        public void RemoveContractLine(int contractId, int contractLineId)
+        {
+            listOfContracts.Find(x => x.Id == contractId).ContractLines.RemoveAll(x => x.Id == contractLineId);
+            //dal.RemoveContractLine(contractLineId);
+        }
+
+}
+}
--- SkrotlogMVC/Startup.cs
+++ SkrotlogMVC/Startup.cs
@@ -24,6 +24,7 @@ namespace SkrotlogMVC
         {
             services.AddMvc();
             services.AddSingleton<CustomerRepository>();
+            services.AddSingleton<ContractRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
